5c0eb2f441090acc63eb9407161c4a798694c935
KasyanAndrey <andreykasyan88@gmail.com>
2021-06-25 17:55:08 +0300

    done HW-11 part-1

--- js/index.js
+++ js/index.js
@@ -7,6 +7,31 @@ const colors = [
   "#795548",
 ];
 
+const refs = {
+  bodyEl: document.querySelector("body"),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+let timerId = null;
+
+refs.startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    let randomIdx = randomIntegerFromInterval(0, colors.length);
+    colors.forEach((num, idx) => {
+      if (randomIdx === idx) {
+        refs.bodyEl.style.backgroundColor = `${num}`;
+        refs.startBtn.setAttribute("disabled", true);
+      }
+    });
+  }, 1000);
+});
+
+refs.stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  refs.startBtn.removeAttribute("disabled");
+});
